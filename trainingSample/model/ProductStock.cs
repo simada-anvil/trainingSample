b23407e915882740267c0ca42cca684da17bd848
simada <simada@anvil.ne.jp>
2021-08-31 18:21:13 +0900

    在庫情報クラスのプロパティが省略形になっていたのでめんどくさくない程度に省略形に修正

--- trainingSample/model/ProductStock.cs
+++ trainingSample/model/ProductStock.cs
@@ -11,7 +11,11 @@ namespace trainingSample.model
     /// </summary>
     class ProductStock
     {
-
+        /// <summary>
+        /// コンストラクタ
+        ///    クラスのインスタンス化時に真っ先に動きメソッド
+        ///    インスタンス化と同時に定義しておきたい情報がある場合はセットしておく。
+        /// </summary>
         public ProductStock()
         {
 
@@ -20,37 +24,93 @@ namespace trainingSample.model
         /// <summary>
         /// No.
         /// </summary>
-        public int no { get; set; }
+        private int _no = 0;
 
         /// <summary>
         /// 商品コード
         /// </summary>
-        public string productCode { get; set; }
+        private string _productCode = string.Empty;
 
         /// <summary>
         /// 商品名
         /// </summary>
-        public string productName { get; set; }
+        private string _productName = string.Empty;
 
         /// <summary>
         /// 単価
         /// </summary>
-        public int price { get; set; }
+        private int _price = 0;
 
         /// <summary>
         /// 数量
         /// </summary>
-        public int quantity { get; set; }
+        private int _quantity = 0;
 
         /// <summary>
-        /// 金額
+        /// 数量
         /// </summary>
-        public int summary { get; set; }
+        private int _summary = 0;
 
         /// <summary>
         /// 登録日付
         /// </summary>
-        public string addDate { get; set; }
+        public string _addDate = string.Empty;
+
+        /// <summary>
+        /// No.
+        /// </summary>
+        public int no {
+            get { return _no; }
+            set { _no = value; }
+        }
+
+        /// <summary>
+        /// 商品コード
+        /// </summary>
+        public string productCode {
+            get {   return _productCode; }
+            set {   _productCode = value; } 
+        }
+
+        /// <summary>
+        /// 商品名
+        /// </summary>
+        public string productName {
+            get {   return _productName; }
+            set {   _productName = value; } 
+        }
+
+        /// <summary>
+        /// 単価
+        /// </summary>
+        public int price {
+            get {   return _price; }
+            set {   _price = value; } 
+        }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int quantity { 
+            get {   return _quantity ; }
+            set {   _quantity = value; } 
+        }
+
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public int summary { 
+            get {   return _summary; } 
+            set {   _summary = value; } 
+        }
+
+        /// <summary>
+        /// 登録日付
+        /// </summary>
+        public string addDate {
+            get { return _addDate; }
+            set { _addDate = value; }
+        }
 
     }
 }
